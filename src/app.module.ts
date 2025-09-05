2dd0b0cfaf8e157e8782ad25fb750afad5579dfa
Firmansyahp16 <firman.jka@gmail.com>
2025-09-05 13:48:09 +0700

    [FIX] Modules and Providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
@@ -8,12 +9,27 @@ import { AuditModule } from './audit/audit.module';
 import { AuthModule } from './auth/auth.module';
 import { BidModule } from './bid/bid.module';
 import { DatabaseModule } from './database/database.module';
+import { QueueModule } from './queue/queue.module';
+import { QueueService } from './queue/queue.service';
+import { RedisModule } from './redis/redis.module';
+import { RedisService } from './redis/redis.service';
 import { UserModule } from './user/user.module';
+import { WorkerModule } from './worker/worker.module';
+import { WorkerService } from './worker/worker.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    BullModule.forRoot({
+      connection: {
+        host: 'redis',
+        port: 6379,
+      },
+      defaultJobOptions: {
+        attempts: 3,
+      },
     }),
     DatabaseModule,
     UserModule,
@@ -22,8 +38,11 @@ import { UserModule } from './user/user.module';
     AuditModule,
     AuthModule,
     JwtModule,
+    RedisModule,
+    QueueModule,
+    WorkerModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, RedisService, QueueService, WorkerService],
 })
 export class AppModule {}
